ea70f90d4e230d15e26dc9fd33152042083012dc
Kirill Korchagin <75925049+Kirill-Korchagin@users.noreply.github.com>
2023-12-02 10:42:50 +0400

    add function to play 1 round of the game and made player selection case-insensitive

--- javascript-index.js
+++ javascript-index.js
@@ -9,3 +9,27 @@ function getComputerChoice() {
     }
     return choice
 }
+
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let computer = computerSelection;
+    if (player === "Rock" && computer === "Paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (player === "Scissors" && computer === "Rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (player === "Paper" && computer === "Scissors") {
+        return "You Lose! Scissors beat Paper";
+    } else if (player === "Paper" && computer === "Rock"){
+        return "You Win! Paper beats Rock";
+    } else if (player === "Rock" && computer === "Scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (player === "Scissors" && computer === "Paper") {
+        return "You Win! Scissors beat Paper";      
+    } else {
+        return "It's a tie, play again!";
+    }
+}
+
+// const playerSelection = "rOCK";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
