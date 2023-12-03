fde369ed95fe12b40aee1c116a700248a962e3af
Kirill Korchagin <75925049+Kirill-Korchagin@users.noreply.github.com>
2023-12-03 15:27:00 +0400

    add game(). Was getting only single prompt because playerSelection and computerSelection were provided only for round1. copied both variables before every single round, otherwise the both variables would get reused in all rounds producing identical results. spent 2 hours thinking why the results were identical, tried several google searches. found help from chatgpt

--- javascript-index.js
+++ javascript-index.js
@@ -26,24 +26,100 @@ function playRound(playerSelection, computerSelection) {
     } else if (player === "Scissors" && computer === "Paper") {
         return "You Win! Scissors beat Paper";
     } else if (player === computer) {
-        return "It's a tie, play again!";
+        return "It's a tie!";
     } else {
         return "Wrong selection, play again!";
     }
 }
 
-let playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
-let computerSelection = getComputerChoice();
-
 function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
+    let computerSelection = getComputerChoice();
+
     let round1 = playRound(playerSelection, computerSelection);
-    let round2 = playRound(playerSelection, computerSelection);
-    let round3 = playRound(playerSelection, computerSelection);
-    let round4 = playRound(playerSelection, computerSelection);
-    let round5 = playRound(playerSelection, computerSelection);
+    
+    if (round1.includes("You Win")) {
+        playerScore += 1;
+    } else if (round1.includes("You Lose")) {
+        computerScore += 1;
+    }
+
     console.log(round1);
+    console.log("Player score: " + playerScore);
+    console.log("Computer score: " + computerScore);
+
+    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
+    computerSelection = getComputerChoice();
+
+    let round2 = playRound(playerSelection, computerSelection);
+    
+    if (round2.includes("You Win")) {
+        playerScore += 1;
+    } else if (round2.includes("You Lose")) {
+        computerScore += 1;
+    }
+
     console.log(round2);
+    console.log("Player score: " + playerScore);
+    console.log("Computer score: " + computerScore);
+
+    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
+    computerSelection = getComputerChoice();
+
+    let round3 = playRound(playerSelection, computerSelection);
+    
+    if (round3.includes("You Win")) {
+        playerScore += 1;
+    } else if (round3.includes("You Lose")) {
+        computerScore += 1;
+    }
+
     console.log(round3);
+    console.log("Player score: " + playerScore);
+    console.log("Computer score: " + computerScore);
+
+    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
+    computerSelection = getComputerChoice();
+
+    let round4 = playRound(playerSelection, computerSelection);
+    
+    if (round4.includes("You Win")) {
+        playerScore += 1;
+    } else if (round4.includes("You Lose")) {
+        computerScore += 1;
+    }
+
+    console.log(round4);
+    console.log("Player score: " + playerScore);
+    console.log("Computer score: " + computerScore);
+
+    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
+    computerSelection = getComputerChoice();
+
+    let round5 = playRound(playerSelection, computerSelection);
+    
+    if (round5.includes("You Win")) {
+        playerScore += 1;
+    } else if (round5.includes("You Lose")) {
+        computerScore += 1;
+    }
+
+    console.log(round5);
+    console.log("Player score: " + playerScore);
+    console.log("Computer score: " + computerScore);
+
+    if (playerScore === computerScore) {
+        console.log("The Match Is A Tie!")
+    } else if (playerScore < computerScore) {
+        console.log("You Lose The Match!");
+    } else {
+        console.log("You Win The Match!");
+    }
+
 }
 
 game();
+
