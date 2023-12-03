6b5b00a331a413aa9f9bca85db070646a2db5920
Kirill Korchagin <75925049+Kirill-Korchagin@users.noreply.github.com>
2023-12-03 21:17:41 +0400

    refactor game() and playRound()
    game() - replace round1-round5 with a while(condition || condition).
    wrong logic, because while terminates only when both conditions are false.
    achieved with while(condition && condition).
    
    playRound() - combined all the loss and win scenarios using ||.
    achieved individual messages by using variables inside message strings.

--- javascript-index.js
+++ javascript-index.js
@@ -13,18 +13,11 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     let computer = computerSelection;
-    if (player === "Rock" && computer === "Paper") {
-        return "You Lose! Paper beats Rock";
-    } else if (player === "Scissors" && computer === "Rock") {
-        return "You Lose! Rock beats Scissors";
-    } else if (player === "Paper" && computer === "Scissors") {
-        return "You Lose! Scissors beat Paper";
-    } else if (player === "Paper" && computer === "Rock"){
-        return "You Win! Paper beats Rock";
-    } else if (player === "Rock" && computer === "Scissors") {
-        return "You Win! Rock beats Scissors";
-    } else if (player === "Scissors" && computer === "Paper") {
-        return "You Win! Scissors beat Paper";
+
+    if ((player ==="Rock" && computer === "Paper") || (player === "Scissors" && computer === "Rock") || (player === "Paper" && computer === "Scissors")) {
+        return ("You Lose! " + computer + " beats " + player);
+    } else if ((player === "Paper" && computer === "Rock") || (player === "Rock" && computer === "Scissors") || (player === "Scissors" && computer === "Paper")) {
+        return ("You Win! " + player + " beats " + computer);
     } else if (player === computer) {
         return "It's a tie!";
     } else {
@@ -36,80 +29,22 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    let playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
-    let computerSelection = getComputerChoice();
+    while (playerScore < 5 && computerScore < 5) {
+        let playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
+        let computerSelection = getComputerChoice();
 
-    let round1 = playRound(playerSelection, computerSelection);
+        let round = playRound(playerSelection, computerSelection);
     
-    if (round1.includes("You Win")) {
-        playerScore += 1;
-    } else if (round1.includes("You Lose")) {
-        computerScore += 1;
+        if (round.includes("You Win")) {
+            playerScore += 1;
+        } else if (round.includes("You Lose")) {
+            computerScore += 1;
+        }
+
+        console.log(round);
+        console.log("Player score: " + playerScore);
+        console.log("Computer score: " + computerScore);  
     }
-
-    console.log(round1);
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-
-    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
-    computerSelection = getComputerChoice();
-
-    let round2 = playRound(playerSelection, computerSelection);
-    
-    if (round2.includes("You Win")) {
-        playerScore += 1;
-    } else if (round2.includes("You Lose")) {
-        computerScore += 1;
-    }
-
-    console.log(round2);
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-
-    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
-    computerSelection = getComputerChoice();
-
-    let round3 = playRound(playerSelection, computerSelection);
-    
-    if (round3.includes("You Win")) {
-        playerScore += 1;
-    } else if (round3.includes("You Lose")) {
-        computerScore += 1;
-    }
-
-    console.log(round3);
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-
-    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
-    computerSelection = getComputerChoice();
-
-    let round4 = playRound(playerSelection, computerSelection);
-    
-    if (round4.includes("You Win")) {
-        playerScore += 1;
-    } else if (round4.includes("You Lose")) {
-        computerScore += 1;
-    }
-
-    console.log(round4);
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-
-    playerSelection = prompt("Choose your hand!", "Rock? Paper? Scissors?");
-    computerSelection = getComputerChoice();
-
-    let round5 = playRound(playerSelection, computerSelection);
-    
-    if (round5.includes("You Win")) {
-        playerScore += 1;
-    } else if (round5.includes("You Lose")) {
-        computerScore += 1;
-    }
-
-    console.log(round5);
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
 
     if (playerScore === computerScore) {
         console.log("The Match Is A Tie!")
